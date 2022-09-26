82f27b7112adb0f4594ad279dfcd36545a9fba61
mxswat <massimocacciapaglia.13@gmail.com>
2022-09-26 19:30:31 +0100

    Nutritionist Trait works!

--- Contents/mods/MxsQoLPack/media/lua/client/magazinesISReadABook.lua
+++ Contents/mods/MxsQoLPack/media/lua/client/magazinesISReadABook.lua
@@ -2,7 +2,7 @@ require "TimedActions/ISReadABook"
 
 local old_ISReadABook_perform = ISReadABook.perform
 function ISReadABook:perform(...)
-    if self.item:getFullType() == "MxsQoLPack.NutritionistMag1" then
+    if self.item:getFullType() == "MxQoLPack.NutritionistMag1" then
         if not self.character:HasTrait("Lucky") then
             self.character:getTraits():add("Nutritionist2");
         end
