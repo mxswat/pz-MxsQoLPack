7237c60e6941510850fee2d509a3987fef8144ee
mxswat <massimocacciapaglia.13@gmail.com>
2022-10-03 22:32:53 +0100

    Added message on getting trait

--- Contents/mods/MxsMagazines/media/lua/client/magazinesISReadABook.lua
+++ Contents/mods/MxsMagazines/media/lua/client/magazinesISReadABook.lua
@@ -5,6 +5,7 @@ function ISReadABook:perform(...)
     if self.item:getFullType() == "MxQoLPack.NutritionistMag1" then
         if not self.character:HasTrait("Lucky") then
             self.character:getTraits():add("Nutritionist2");
+            HaloTextHelper.addTextWithArrow(self.character, 'Nutritionist Trait', true, HaloTextHelper.getColorGreen())
         end
 	end
 	return old_ISReadABook_perform(self, ...)
